bdad48a3fa7679694e8d5cc62b77b3b374a6f63b
Evandro <evandrocalado07@gmail.com>
2022-12-11 01:55:52 -0300

    update

--- evandro-calado/src/sanity.js
+++ evandro-calado/src/sanity.js
@@ -1,12 +1,11 @@
-import { createClient } from 'next-sanity';
+import { createClient } from "next-sanity";
 
 const client = createClient({
-    projectId: 'q6b42ka4',
-    dataset: 'production',
-    apiVersion: '2022-12-11',
-    useCdn: false,
-    token: 'skbsnqlaMnpFkgIh85TOgd4U74WlCuk0GIrs7YaP7ryon753K2dmpVzTsTZC5o7VZSI3ylYPfxFz6jF8wXxkpp2nnXXZznmrui2GhTPnUHNFUPoZAlpXRnTSZfw4Dzjaz05f7FN1cpvGNkZd7vv1kK1VHdFb1UIiSNJwzJTlw9twMvT0MwRi',
-    ignoreBrowserTokenWarning: true
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_DATASET,
+  apiVersion: process.env.NEXT_PUBLIC_API_VERSION,
+  useCdn: process.env.NEXT_PUBLIC_USE_CDN,
+  token: process.env.NEXT_PUBLIC_TOKEN,
 });
 
 export default client;
